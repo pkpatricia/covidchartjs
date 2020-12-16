484e65f5f1d625a646ccbf1c5343c366fb319a8f
pkpatricia <pk.patricia@gmail.com>
2020-12-15 23:31:10 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+var ctx = document.getElementById('myChart');
+
+// Get the data
+d3.json("https://api.covidtracking.com/v1/states/current.json").then(function(data) {
+
+    let states = [];
+    let percentages = [];
+
+    // Sort the Data from Largest to Smallest
+    data.sort((a, b) => ((b.positive / b.totalTestResults) * 100) - ((a.positive / a.totalTestResults) * 100));
+
+    data.forEach((data) => {
+        states.push(data.state);
+        percentages.push(((data.positive / data.totalTestResults) * 100).toPrecision(3));
+        console.log("The state of " + data.state + " currently has a positive testing percentage of: " + ((data.positive / data.totalTestResults) * 100).toPrecision(3) + "%.");
+    });
+
+    var myBarChart = new Chart(ctx, {
+        type: 'bar',
+        data:{
+            labels: states,
+            datasets: [{
+                label: 'Daily Positive Covid Test Results',
+                data: percentages,
+                backgroundColor:
+                    'rgba(255, 99, 132, 0.2)',
+                borderColor:
+                    'rgba(255, 99, 132, 1)',
+                borderWidth: 3
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: 'True',
+                        max: 26,
+                        min: 0,
+                        stepSize: 2
+                    }
+                }]
+            },
+            legend: {
+                display: true,
+                labels: {
+                    fontColor: 'rgb(255, 99, 132)'
+                }
+            }
+        }
+    });
+});
